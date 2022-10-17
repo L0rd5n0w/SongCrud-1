2709f20c058fefb4f344c3302154e0973fe203b7
L0rd_5n0w <ifeoluwaadewusi04@gmail.com>
2022-10-17 05:31:23 -0700

    New Update

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,4 +1,8 @@
+from email.policy import default
+from random import choices
+from secrets import choice
 from tkinter import CASCADE
+from turtle import title
 import uuid
 from django.db import models
 import uuid
@@ -10,20 +14,29 @@ class Artist(models.Model):
     first_name = models.CharField(max_length=100 ,blank=True)
     last_name = models.CharField(max_length=100 , blank=True)
     age = models.IntegerField(blank=True)
-    id = models.UUIDField(default=uuid.uuid3, unique=True, primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.first_name
 
 
 class Song(models.Model):
     title = models.CharField(blank=True, max_length=40)
-    date_released = models.DateTimeField(blank=True, auto_now_add=True)
+    date_released = models.DateTimeField(blank=True, default=datetime.today)
     artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE)
     VOTE_TYPE = (
         ('likes', 'Like'),
         ('Dis', 'Dislike')
     )
-    id = models.UUIDField(default=uuid.uuid3, unique=True, primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
 
 class Lyric(models.Model):
     content = models.CharField(blank=True, max_length=5000)
     song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
-    id = models.UUIDField(default=uuid.uuid3, unique=True, primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.song_id.title
